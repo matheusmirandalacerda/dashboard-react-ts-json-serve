4ffc56e2ac5b51db8c69340ec62ab378f9324e6d
Matheus <108967989+matheusmirandalacerda@users.noreply.github.com>
2023-05-22 10:06:24 -0300

    Delete services directory

--- services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'https://json-server-ex-gq2u.onrender.com',
-    //baseURL: 'http://localhost:5000',
-});
-
-export default api;
